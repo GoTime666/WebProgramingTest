78fc6c4ac2e4dc79cdc5ef848d2257488472f3b2
GoTime666 <2811660875@qq.com>
2020-02-08 14:21:49 +0800

    Gotime2

--- WebProgramingTest/src/tcp/server/TCPServer.java
+++ WebProgramingTest/src/tcp/server/TCPServer.java
@@ -22,7 +22,7 @@ public class TCPServer {
 
 	@Test
 	public void fun1() throws IOException {
-		//111
+		//Gotime2
 		ServerSocket serverSocket = new ServerSocket(8888);
 		Socket clientSocket = serverSocket.accept();
 		InetAddress ipObject = clientSocket.getInetAddress();
